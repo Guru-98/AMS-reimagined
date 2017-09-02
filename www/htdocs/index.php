bb53b0052fea8217dc1d55d3f6c8b189da0675a4
Guru Prasath G <guru98@live.com>
2017-09-02 21:11:33 +0530

    Added login page

---
+++ www/htdocs/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <title> AMS </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/awesome-bootstrap-checkbox/0.3.7/awesome-bootstrap-checkbox.min.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 col-centered">
+            <div class="login-panel">
+                <h4 class="login-panel-title">Sitename</h4>
+                <p class="login-panel-tagline">Sitename helps you connect and share with the people in your life.</p>
+                <div class="login-panel-section">
+                    <div class="form-group">
+                        <div class="input-group margin-bottom-sm">
+                            <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw" aria-hidden="true"></i></span>
+                            <input class="form-control" type="text" placeholder="Email address">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-key fa-fw" aria-hidden="true"></i></span>
+                            <input class="form-control" type="password" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="checkbox checkbox-circle checkbox-success checkbox-small">
+                        <input type="checkbox" id="checkbox1">
+                        <label for="checkbox1">Keep me logged in</label>
+                        <a href="#" class="pull-right">Forget your password?</a>
+                    </div>
+                </div>
+                <div class="login-panel-section">
+                    <button type="submit" class="btn btn-default"><i class="fa fa-sign-in fa-fw" aria-hidden="true"></i> Login</button>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+
+</body>
+</html>
